f7db73ab8a6ee359bd24098579915cadfe7d979d
MarcinMaster <m.luba505@gmail.com>
2022-06-27 07:59:46 +0200

    Added an API Fetch combined by search qoute

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import SearchBar from "./components/search/SearchBar";
 import BeerList from "./components/beer/BeerList";
 
 
-function App() {
+const api = {
+  base: "https://api.punkapi.com/v2/beers",
+};
+
+const App = () => {
+  const fetchBeers = async (search) => {
+    const response = await fetch(`${api.base}?beer_name=${search}&per_page=80`);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <Navbar/>
-      <SearchBar/>
-      <BeerList/>
+      <Navbar />
+      <SearchBar fetchBeers={fetchBeers} />
+      <BeerList />
+      <button onClick={fetchBeers}>FETCH</button>
     </div>
   );
-}
+};
 
 export default App;
