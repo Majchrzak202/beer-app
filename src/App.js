8a243a702b89fe0d3979d3c59ece16d33a43bb6e
MarcinMaster <m.luba505@gmail.com>
2022-06-29 06:52:57 +0200

    Added a style css for the whole App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import SearchBar from "./components/search/SearchBar";
 import BeerList from "./components/beer/BeerList";
-
+import "./App.css";
 
 const api = {
   base: "https://api.punkapi.com/v2/beers",
 };
 
 const App = () => {
+  const [beers, setBeers] = useState([]);
+
   const fetchBeers = async (search) => {
     const response = await fetch(`${api.base}?beer_name=${search}&per_page=80`);
     const data = await response.json();
-    console.log(data);
+    setBeers(data);
   };
 
   return (
-    <div className="App">
+    <div>
       <Navbar />
-      <SearchBar fetchBeers={fetchBeers} />
-      <BeerList />
-      <button onClick={fetchBeers}>FETCH</button>
+      <div className="App">
+        <SearchBar fetchBeers={fetchBeers} />
+        <BeerList beers={beers} />
+      </div>
     </div>
   );
 };
