a7869cf50a4c4c87a5eb8ac8288450aca5ff0a57
MarcinMaster <m.luba505@gmail.com>
2022-07-05 06:28:22 +0200

    Added a Dynamic Route into the BeerPage using useParams

--- src/components/beer/BeerPage.js
+++ src/components/beer/BeerPage.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const BeerPage = ({beers}) => {
+const BeerPage = () => {
+  const [beer, setBeer] = useState();
+  const { id } = useParams();
 
-   
-    
-    return <div>
-        
-    </div>
-}
+  useEffect(() => {
+    const fetchBeer = async () => {
+      const response = await fetch(`https://api.punkapi.com/v2/beers/${id}`);
+      const data = await response.json();
+      setBeer(data);
+      console.log(data);
+    };
+
+    fetchBeer();
+  }, [id]);
+
+  if (typeof beer === "undefined") {
+    return null;
+  }
+  const name = beer[0].name;
+  const description = beer[0].description;
+
+  return <div>{name}{description}</div>;
+  /* if (typeof beer === "undefined") {
+    return null;
+  } else {
+    return (
+      <div>
+        {beer[0].name} <br />
+        {beer[0].abv}
+      </div>
+    );
+  } */
+};
 
 export default BeerPage;
