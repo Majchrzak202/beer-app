0b424eb1e2cb367fdff8136650eb2698b1aef21b
MarcinMaster <m.luba505@gmail.com>
2022-07-13 17:34:58 +0200

    Added a switch function to manage filtering, filtering Logic, and placed it in useEffect

--- src/components/beer/BeerList.js
+++ src/components/beer/BeerList.js
@@ -1,32 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BeerItem from "./BeerItem";
 import { Link } from "react-router-dom";
 
 import "./BeerList.css";
 
 const BeerList = ({ beers }) => {
+  const [status, setStatus] = useState("Most Popular");
+  const [filteredBeers, setFilteredBeers] = useState([]);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "Most Popular":
+          setFilteredBeers(beers);
+          break;
+        case "Lowest ABV":
+          setFilteredBeers([...beers].sort((a, b) => a.abv - b.abv));
+          break;
+        case "Lowest IBU":
+          setFilteredBeers([...beers].sort((a, b) => a.ibu - b.ibu));
+          break;
+        case "Highest ABV":
+          setFilteredBeers([...beers].sort((a, b) => b.ibu - a.ibu));
+          break;
+        case "Highest IBU":
+          setFilteredBeers([...beers].sort((a, b) => b.ibu - a.ibu));
+          break;
+        default:
+          setFilteredBeers(beers);
+          break;
+      }
+    };
+    filterHandler();
+  }, [status, beers]);
+
   if (beers.length === 0) {
     return null;
   }
 
-  console.log(beers);
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
 
   return (
     <div className="beer-list">
+      <div>
+        {" "}
+        <button>Sort</button>
+        Sort by :
+        <select onChange={statusHandler}>
+          <option>Most Popular</option>
+          <option>Lowest ABV</option>
+          <option>Lowest IBU</option>
+          <option>Highest ABV</option>
+          <option>Highest IBU</option>
+        </select>
+      </div>
       <div className="line"></div>
       <ul>
-        {beers.map((beer) => (
-          
+        {filteredBeers.map((beer) => (
           <Link key={beer.id} to={`/beer/${beer.id}`}>
-          <BeerItem
-            name={beer.name}
-            abv={beer.abv}
-            ibu={beer.ibu}
-            key={beer.id}
-            img={beer["image_url"]}
-            id={beer.id}
-            tagline={beer.tagline}
-          />
+            <BeerItem
+              name={beer.name}
+              abv={beer.abv}
+              ibu={beer.ibu}
+              key={beer.id}
+              img={beer["image_url"]}
+              id={beer.id}
+              tagline={beer.tagline}
+            />
           </Link>
         ))}
       </ul>
