d0b1b86133dbb0bcd1bcad4019ad5264674d2f4d
MarcinMaster <m.luba505@gmail.com>
2022-08-09 06:15:37 +0200

    Added a Beer logo in the ABout section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './About.css'
+import beerLogo from './../assets/beerLogo.png'
 
 const About = () => {
     return <div className="about">
+        <img src={beerLogo} alt='beerLogo' />
         <h3>This is an Educational purpose Beer App. It is made with React.</h3>
         <p>You can search ANY Beer in Database, filter by IBU, ABV and check all the deatils of this beer!</p>
         <p>Also check out food paring examples!</p>
