9abc8f78eb715573bb9b246528c49056f11f12df
MarcinMaster <m.luba505@gmail.com>
2022-08-12 12:44:55 +0200

    Added a Navbar MUI component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,43 +1,138 @@
 import React, { useState } from "react";
-import "./Navbar.css";
+import {
+  Menu,
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Container,
+  Button,
+  MenuItem,
+  CardMedia,
+} from "@mui/material";
+
+import { Menu as MenuIcon } from "@material-ui/icons";
+import useStyles from "./Styles";
 import { Link } from "react-router-dom";
-import beerLogo from "../../assets/beerLogo.png"
+import beerLogo from "../../assets/beerLogo.png";
+
+const pages = [
+  <Link style={{ color: "black", textDecoration: "none" }} to="/">
+    Home
+  </Link>,
+  <Link style={{ color: "black", textDecoration: "none" }} to="/about">
+    About
+  </Link>,
+  <Link style={{ color: "black", textDecoration: "none" }} to="/contact">
+    Contact
+  </Link>,
+  <Link style={{ color: "black", textDecoration: "none" }} to="/randombeer">
+    Random Beer
+  </Link>,
+];
 
 const Navbar = () => {
-  const [navExpanded, setNavExpanded] = useState(false);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const classes = useStyles();
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
-    <nav className="navbar">
-      <img className="brand" src={beerLogo} alt="1" />
-      <button
-        onClick={() => {
-          setNavExpanded(!navExpanded);
-        }}
-        className="hamburger"
+    <div>
+      <AppBar
+        sx={{ backgroundColor: "hsla(0, 100%, 50%, 0.15)" }}
+        position="fixed"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      <div className={navExpanded ? "nav-menu expanded" : "nav-menu"}>
-        <ul >
-          <li><Link className="link" to={'/'}>Home</Link></li>
-          <li><Link className="link" to={'/about'}>About</Link></li>
-          <li><Link className="link" to={'/contact'}>Contact</Link></li>
-          <li><Link className="link" to={'/randombeer'}>Random Beer</Link></li>
-        </ul>
-      </div>
-    </nav>
+        <Container maxWidth="xl">
+          <Toolbar className={classes.toolbar} disableGutters>
+            <Link to="/">
+              <CardMedia
+                sx={{
+                  display: { xs: "none", md: "flex" },
+                }}
+              >
+                <img
+                  style={{ width: "50px", height: "50px" }}
+                  src={beerLogo}
+                  alt="1"
+                />
+              </CardMedia>
+            </Link>
+
+            <Link to="/">
+              <CardMedia
+                sx={{
+                  display: { xs: "flex", md: "none" },
+                }}
+              >
+                <img
+                  style={{ width: "50px", height: "50px" }}
+                  src={beerLogo}
+                  alt="1"
+                />
+              </CardMedia>
+            </Link>
+
+            <div className={classes.grow} />
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={Math.random()}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={Math.random()} onClick={handleCloseNavMenu}>
+                    <Typography>{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
   );
 };
-
 export default Navbar;
